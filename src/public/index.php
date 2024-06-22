3186f49687cb8484120666e9e29dd2c1901e6a3a
atsuki-kitahori <pcp.m.t15@gmail.com>
2024-06-22 11:06:08 +0900

    5-1基礎課題②追加

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+/*エラー1*/
 $spendings = ['apple' => 100, 'orange' => 150];
 echo findTotalSpendings($spendings);
 
@@ -10,4 +11,19 @@ function findTotalSpendings($spendings)
         $totalSpendings += $spending;
     }
     return $totalSpendings;
+}
+echo '<br>';
+
+/*エラー2*/
+$spendings = ['apple' => 100, 'orange' => 150];
+$incomes = ['okozukai' => 10000, 'partTimeSalary' => 35000];
+echo findTotalIncomes($incomes);
+
+function findTotalIncomes($incomes)
+{
+    $totalIncomes = 0;
+    foreach ($incomes as $income) {
+        $totalIncomes += $income;
+    }
+    return $totalIncomes;
 }
